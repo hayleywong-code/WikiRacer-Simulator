c2171fc82975b2a7e022697abc93db8e1ac1a815
hayleywong-code <hlawong92002@gmail.com>
2023-01-15 20:34:36 -0700

    Completed automated WikiRacer simulator.

--- WikiRacer.py
+++ WikiRacer.py
@@ -1,55 +1,82 @@
 # import required modules
 from bs4 import BeautifulSoup
 import requests
-import Hyperlink
+
+wikiLinkStart = "https://en.wikipedia.org"
 
 
-def getNeighbors(link: Hyperlink.Hyperlink) -> list:
+def getNeighbors(link: str) -> list:
     # get URL
-    page = requests.get(link.getFullWikiLink())
+    try:
+        page = requests.get(wikiLinkStart + link)
+    except:
+        return []
 
     # scrape webpage
     soup = BeautifulSoup(page.content, "html.parser")
 
     # get page content and find all <a> elements
     pageContent = soup.find(id="bodyContent")
-    pElements = pageContent.find_all("p")
+    try:
+        pElements = pageContent.find_all("p")
+    except:
+        return []
     aElements = []
 
+    # add <a> elements to the aElements list only if they have a title attribute
     for p in pElements:
         for a in p.find_all("a"):
             if a.get("title") != None:
-                title = a.get("title")
-                href = a.get("href")
-
-                hyperlink = Hyperlink.Hyperlink(title, href, a)
-
-                aElements.append(hyperlink)
+                aElements.append(a.get("href"))
 
     return aElements
 
 
-def bfs(firstLink: Hyperlink.Hyperlink, secondLink: Hyperlink.Hyperlink) -> bool:
+def tracePath(parent: dict, firstLink: str, secondLink: str):
 
-    print(f"Beginning search for {secondLink} from {firstLink}.")
+    path = [secondLink]
 
+    while True:
+        path.append(parent[secondLink])
+        secondLink = path[-1]
+
+        if firstLink == secondLink:
+            path.reverse()
+            return path
+
+
+def bfs(firstLink: str, secondLink: str) -> bool:
+
+    print(f"Beginning search from {firstLink[6:]} to {secondLink[6:]}...")
+
+    queue = []
+    queries = 0
+    marked = set()
     parent = {}
     queue.append(firstLink)
 
     while queue:
         link = queue.pop(0)
-        print(link)
         current_parent = link
         neighbors = getNeighbors(link)
 
         for n in neighbors:
-            if n in marked:
+            if n not in marked:
                 parent[n] = current_parent
                 marked.add(n)
                 queue.append(n)
-            if n.__eq__(secondLink):
-                print("connection found")
-                print(parent)
+                queries += 1
+            if n == secondLink:
+                print(f"Connection found after {queries} queries")
+                print(
+                    "============================================================================="
+                )
+                print("Printing path below:")
+                path = tracePath(parent, firstLink, secondLink)
+                for l in path:
+                    print(l[6:])
+                return True
+    return False
 
 
 # get user input
@@ -63,15 +90,9 @@ print()
 
 startingURL = input("Please enter your starting Wikipedia URL: ")
 startingURL = startingURL[24:]
-startingHyperlink = Hyperlink.Hyperlink(startingURL[6:], startingURL, None)
 
 endingURL = input("Please enter your ending Wikipedia URL: ")
 endingURL = endingURL[24:]
-endingHyperlink = Hyperlink.Hyperlink(endingURL[6:], endingURL, None)
 
-queue = []
-marked = set()
-
-
-print("****************************************************")
-bfs(startingHyperlink, endingHyperlink)
+print("*****************************************************************************")
+bfs(startingURL, endingURL)
